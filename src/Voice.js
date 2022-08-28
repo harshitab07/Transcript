786001017705dd284678b0eaebc1e6bc07c9c3a2
Harshub07 <95317276+harshitab07@users.noreply.github.com>
2022-08-28 21:12:06 +0530

    my first commit

---
+++ src/Voice.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import './Voice.css';
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = 'en-US';
+
+
+function Voice() {
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState(null);
+  const [savedNotes, setSavedNotes] = useState([]);
+
+  useEffect(()=>{
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if(isListening){
+      mic.start()
+      mic.onend = () => {
+        console.log('continue..')
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend=()=>{
+        console.log('Stopped mic on click')
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mic is ON')
+    }
+    mic.onresult = event => {
+      const transcript = Array.from(event.results).map(result => result[0]).map(result => result.transcript).join('')
+      console.log(transcript)
+      setNote(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
+
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
+
+  return (
+    <>
+    <img id='bg' src='https://takenote.co/wp-content/uploads/2019/12/dissertation%20interview%20transcription%20guide%20to%20transcription%20services.jpg' />
+    <img id='bg-med' src='https://img.freepik.com/premium-photo/stack-books-with-headphones-tablets-wooden-background-concept-modern-technological-education-study-books_243391-991.jpg?w=2000' />
+    <img id='bg-sml' src='https://img.lovepik.com/background/20211101/medium/lovepik-indoor-study-mobile-phone-wallpaper-background-image_400618844.jpg' />
+    <h1>Transcript</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Speak Up</h2><br></br>
+          {isListening ? <span id='icon'>🎤</span> : <span id='icon'>🔇</span>}
+          <div className="buttons">
+          <button onClick={()=> setIsListening(prevState => !prevState)}>{isListening ? <span>Stop</span> : <span>Start</span>}</button>
+            <button onClick={handleSaveNote} disabled={!note}>Save</button>
+            <button onClick={()=>setNote('')} disabled={!note}>🗘</button>
+          </div>
+          <p className='recordedNotes'>{note}</p>
+        </div>
+        <div className='box' id='trans-box'>
+          <h2 id='notes'>Transcripted</h2>
+          {savedNotes.map(n => (<p key={n} className='recordedNotes'>{n}</p>))}
+        </div>
+      </div>
+    </>
+
+  );
+}
+
+export default Voice; 
